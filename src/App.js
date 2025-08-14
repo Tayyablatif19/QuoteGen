83482d2f094e5c7f15071b91bcd99ed24868537a
Tayyab Latif <mlatif.bee21seecs@seecs.edu.pk>
2025-08-15 02:06:59 +0500

    Remove unused assets and clean up project structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const [quotes, setQuotes] = useState([]);
+
+
+
+  const URL = 'https://api.api-ninjas.com/v1/quotes'
+
+  const quoteSearch = async () => {
+    const response = await fetch(URL, {
+      headers: { 'X-Api-Key': 'UO4xN9m8vh4/k12/VR7g+g==LGIYbjdPM4gOEFNi' }
+
+    })
+    const data = await response.json()
+
+    setQuotes(data);
+  }
+
+
+  useEffect(() => {
+    quoteSearch();
+  }, [])
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='container'>
+
+        {
+          quotes.length > 0
+            ? (
+              <div className='quote'>
+                <p>{quotes[0].quote}</p>
+                <p>{quotes[0].author}</p>
+
+              </div>
+
+            ) : (
+
+              <div>
+                <h4>No Quote to show</h4>
+                
+              </div>
+
+            )
+        }
+
+
+      
+      </div>
+      <button onClick={() => {
+        quoteSearch(quotes);
+      }}>Generate quote</button>
+
     </div>
+    </>
   );
 }
 
